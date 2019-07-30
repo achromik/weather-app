68db59afef063eb07dca4049d19a97044d210587
Aleksander Chromik <achromik@gmail.com>
2019-07-30 18:38:23 +0200

    Prepare the homepage

--- frontend/src/components/homePage/HomePage.jsx
+++ frontend/src/components/homePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import { Input } from 'antd';
+import { sizes, breakpoints } from 'src/config/variables';
 
 import { fetchCityWeather } from 'src/features/weather/actions/weatherActions';
 import {
@@ -10,29 +12,39 @@ import {
     weatherFetchedSuccessfullySelector,
 } from 'src/features/weather/selectors/weatherSelectors';
 import { ErrorBox } from 'src/common/components/ErrorBox/ErrorBox';
+import { Weather } from 'src/components/weather/Weather';
 
 const { Search } = Input;
 
+const Wrapper = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: ${sizes.padding.xs};
+
+    ${breakpoints.md} {
+        padding: ${sizes.padding.lg};
+    }
+`;
+
 export const HomePage = ({ isFetching, fetchedSuccessfully, data, error, fetchCityWeather }) => {
     return (
-        <>
-            <h2>Home page</h2>
+        <Wrapper>
+            <h2>Weather App</h2>
             <Search
                 size="large"
-                placeholder="large size"
+                placeholder="Enter city name..."
                 style={{ width: 200 }}
                 allowClear
                 onSearch={value => fetchCityWeather(value)}
             />
             {isFetching && <div>Loading data...</div>}
-            {fetchedSuccessfully && data && (
-                <div>
-                    <p>City: {data.name}</p>
-                    <p>Clouds: {data.clouds.all}%</p>
-                </div>
-            )}
+            {fetchedSuccessfully && data && <Weather />}
             {error && error.code !== 200 && <ErrorBox message={error.body.message} />}
-        </>
+        </Wrapper>
     );
 };
 
