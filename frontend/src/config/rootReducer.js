1995bbf09566a4aaa93ece1fa48ab314b82eda59
Aleksander Chromik <achromik@gmail.com>
2019-08-01 21:03:58 +0200

    Add weathersList reduce

--- frontend/src/config/rootReducer.js
+++ frontend/src/config/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
 import { weather } from 'src/features/weather/reducer/weatherReducer';
+import { weathersList } from 'src/features/weather/reducer/weathersListReducer';
 
-export const rootReducer = combineReducers({ weather });
+export const rootReducer = combineReducers({ weather, weathersList });
--- frontend/src/features/weather/reducer/weathersListReducer.js
+++ frontend/src/features/weather/reducer/weathersListReducer.js
@@ -1,7 +1,7 @@
 import {
-    FETCH_LOCATION_WEATHER_BEGIN,
-    FETCH_LOCATION_WEATHER_SUCCESS,
-    FETCH_LOCATION_WEATHER_FAILURE,
+    FETCH_CITY_WEATHER_BEGIN,
+    FETCH_CITY_WEATHER_SUCCESS,
+    FETCH_CITY_WEATHER_FAILURE,
 } from '../actions/weatherActions';
 
 const initialState = {
@@ -11,15 +11,15 @@ const initialState = {
     error: undefined,
 };
 
-export const weather = (state = initialState, action) => {
+export const weathersList = (state = initialState, action) => {
     switch (action.type) {
-        case FETCH_LOCATION_WEATHER_BEGIN:
+        case FETCH_CITY_WEATHER_BEGIN:
             return {
                 ...initialState,
                 isFetching: true,
             };
 
-        case FETCH_LOCATION_WEATHER_SUCCESS:
+        case FETCH_CITY_WEATHER_SUCCESS:
             return {
                 ...state,
                 isFetching: false,
@@ -27,7 +27,7 @@ export const weather = (state = initialState, action) => {
                 data: action.payload,
             };
 
-        case FETCH_LOCATION_WEATHER_FAILURE:
+        case FETCH_CITY_WEATHER_FAILURE:
             return {
                 ...state,
                 isFetching: false,
